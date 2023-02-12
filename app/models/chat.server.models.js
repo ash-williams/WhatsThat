32879d5857bcdccc11ce1a57d5a9ae89489a2630
Ashley Williams <55135751@ad.mmu.ac.uk>
2023-02-12 13:38:59 +0000

    Fixed GET /chats so that it actually works now - hopefully

--- app/models/chat.server.models.js
+++ app/models/chat.server.models.js
@@ -205,65 +205,152 @@ const remove_user_from_chat = (chat_id, user_id, done) => {
 
 
 
-const get_chat_info = (user_id) => {
-    return new Promise((resolve, reject) => {
+// const get_chat_info = (user_id) => {
+//     return new Promise((resolve, reject) => {
 
-        const query = `SELECT cu.chat_id, c.name, c.creator, u.user_id, u.given_name, u.family_name, u.email, m.message_id, m.timestamp, m.message, m.author, mu.user_id AS "author_user_id", mu.given_name AS "author_first_name", mu.family_name AS "author_last_name", mu.email AS "author_email" 
-        FROM whatsthat_chat_users cu, whatsthat_chats c, whatsthat_users u, whatsthat_messages m, whatsthat_users mu
-        WHERE cu.user_id = ?
-        AND cu.chat_id = c.chat_id
-        AND c.creator = u.user_id
-        AND m.chat_id = c.chat_id  
-        AND m.author = mu.user_id
-        GROUP BY c.chat_id
-        HAVING MAX(m.timestamp)            
+//         const query = `SELECT cu.chat_id, c.name, c.creator, u.user_id, u.given_name, u.family_name, u.email, m.message_id, m.timestamp, m.message, m.author, mu.user_id AS "author_user_id", mu.given_name AS "author_first_name", mu.family_name AS "author_last_name", mu.email AS "author_email" 
+//         FROM whatsthat_chat_users cu, whatsthat_chats c, whatsthat_users u, whatsthat_messages m, whatsthat_users mu
+//         WHERE cu.user_id = ?
+//         AND cu.chat_id = c.chat_id
+//         AND c.creator = u.user_id
+//         AND m.chat_id = c.chat_id  
+//         AND m.author = mu.user_id
+//         GROUP BY c.chat_id
+//         HAVING MAX(m.timestamp)            
+//         `;
+
+//         let results = [];
+
+//         db.all(query, [user_id], (err, rows) => {
+//             console.log(err, rows)
+
+//             if(err) reject(err)
+
+//             rows.forEach((row) => {
+//                 results.push({
+//                     "chat_id": row.chat_id,
+//                     "name": row.name,
+//                     "creator": {
+//                         "user_id": row.user_id,
+//                         "first_name": row.given_name,
+//                         "last_name": row.family_name,
+//                         "email": row.email
+//                     },
+//                     "last_message": {
+//                         "message_id": row.message_id,
+//                         "timestamp": row.timestamp,
+//                         "message": row.message,
+//                         "author": {
+//                             "user_id": row.author_user_id,
+//                             "first_name": row.author_first_name,
+//                             "last_name": row.author_last_name,
+//                             "email": row.author_email
+//                         }
+//                     }
+
+//                 })
+
+//             })
+//             resolve(results)
+//         })
+//     })
+// }
+
+// const get_all_chats = (user_id, done) => {
+//     get_chat_info(user_id)
+//     .then((data) => {
+//         return done(null, data)
+//     })
+//     .catch((err) => {
+//         return done(err, null)
+//     })
+// }
+
+const get_last_message = (chat_id) => {
+    return new Promise((resolve, reject) => {
+        let last_message_query = `
+            SELECT m.message_id, m.timestamp, m.message, m.author, mu.user_id AS "author_user_id", mu.given_name AS "author_first_name", mu.family_name AS "author_last_name", mu.email AS "author_email" 
+            FROM whatsthat_messages m, whatsthat_users mu
+            WHERE m.chat_id = ?
+            AND mu.user_id = m.author
+            GROUP BY m.chat_id
+            HAVING MAX(m.timestamp)
         `;
 
-        let results = [];
+        db.get(last_message_query, [chat_id], (err, last_message_details) => {
+            if(err) return reject(err)
 
-        db.all(query, [user_id], (err, rows) => {
-            console.log(err, rows)
-
-            if(err) reject(err)
-
-            rows.forEach((row) => {
-                results.push({
-                    "chat_id": row.chat_id,
-                    "name": row.name,
-                    "creator": {
-                        "user_id": row.user_id,
-                        "first_name": row.given_name,
-                        "last_name": row.family_name,
-                        "email": row.email
-                    },
-                    "last_message": {
-                        "message_id": row.message_id,
-                        "timestamp": row.timestamp,
-                        "message": row.message,
-                        "author": {
-                            "user_id": row.author_user_id,
-                            "first_name": row.author_first_name,
-                            "last_name": row.author_last_name,
-                            "email": row.author_email
-                        }
+            if(!last_message_details || last_message_details === null){
+                resolve({})
+            }else{
+                resolve({
+                    "message_id": last_message_details.message_id,
+                    "timestamp": last_message_details.timestamp,
+                    "message": last_message_details.message,
+                    "author": {
+                        "user_id": last_message_details.author_user_id,
+                        "first_name": last_message_details.author_first_name,
+                        "last_name": last_message_details.author_last_name,
+                        "email": last_message_details.author_email
                     }
-
                 })
-
-            })
-            resolve(results)
+            }
         })
     })
 }
 
 const get_all_chats = (user_id, done) => {
-    get_chat_info(user_id)
-    .then((data) => {
-        return done(null, data)
-    })
-    .catch((err) => {
-        return done(err, null)
-    })
+    let chats = [];
+    let errors = [];
+
+    let chats_query = `
+        SELECT c.chat_id, c.name, c.creator, u.user_id, u.given_name, u.family_name, u.email
+        FROM whatsthat_chats c, whatsthat_users u
+        WHERE c.chat_id IN (SELECT chat_id from whatsthat_chat_users WHERE user_id = ?)
+        AND c.creator = u.user_id`;
+
+    db.each(
+        chats_query, 
+        [user_id],
+        (err, chat_details) => {
+            if(err) errors.push(err);
+
+            let chat_object = {
+                "chat_id": chat_details.chat_id,
+                "name": chat_details.name,
+                "creator": {
+                    "user_id": chat_details.user_id,
+                    "first_name": chat_details.given_name,
+                    "last_name": chat_details.family_name,
+                    "email": chat_details.email
+                }
+            }
+
+            chats.push(chat_object)
+            
+        },
+        (err) => {
+            if(err) return done(err, null);
+            if(errors.length > 0) return done(errors, null)
+
+            let counter = 0;
+
+            chats.forEach((chat) => {
+                get_last_message(chat.chat_id)
+                .then((last_message) => {
+                    chat["last_message"] = last_message
+
+                    counter ++;
+                    if(counter === chats.length){
+                        return done(null, chats)
+                    }
+                })
+                .catch((err) => {
+                    return done(err, null)
+                })
+            }) 
+        }
+    )
 }
 
 
